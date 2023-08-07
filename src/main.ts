57759a53d255239ed5040a2a0233e4492179becb
Joey Kuo <joeyskuo@gmail.com>
2023-08-06 20:33:28 -0700

    initial Module and bootstrap method

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -8,3 +9,18 @@ class AppController {
         return 'hello from nest';
     }
 }
+
+@Module({
+    controllers: [AppController]
+})
+class AppModule {
+
+}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(3000);
+}
+
+bootstrap();
